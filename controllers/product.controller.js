5692ef8edbd1632bbb40d6e6ac1cb06e8312a968
robinhooda <robinhooda66@gmail.com>
2021-06-05 01:22:48 +0530

    Refactor. added controller for products routes

---
+++ controllers/product.controller.js
@@ -0,0 +1,63 @@
+const Product = require('../models/Product')
+const { extend } = require('lodash')
+
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find()
+    res.json({ success: true, products })
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: 'unable to get products',
+      errorMessage: err.message,
+    })
+  }
+}
+
+const getProductById = (req, res) => {
+  const { product } = req
+  res.json({ success: true, product })
+}
+
+const updateProducts = async (req, res) => {
+  const product = new Product(req.body)
+  try {
+    const savedProduct = await product.save()
+    res.json({ Product: savedProduct, success: true })
+  } catch (err) {
+    res.json({ success: false, message: err.message })
+  }
+}
+
+const updateproductById = async (req, res) => {
+  const updatedProduct = req.body
+  let { product } = req
+
+  try {
+    product = extend(product, updatedProduct)
+    product = await product.save()
+    res.json({ success: true, product: product })
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: 'error while updating product',
+      error: err.message,
+    })
+  }
+}
+
+const deleteProductById = async (req, res) => {
+  let { product } = req
+
+  await product.remove()
+  product.deleted = true
+  res.json({ success: true, product })
+}
+
+module.exports = {
+  getAllProducts,
+  getProductById,
+  updateProducts,
+  updateproductById,
+  deleteProductById
+}
--- routes/products.router.js
+++ routes/products.router.js
@@ -2,32 +2,21 @@ const express = require('express')
 const router = express.Router()
 const Product = require('../models/Product')
 const { extend } = require('lodash')
+const {
+  getAllProducts,
+  getProductById,
+  updateProducts,
+  updateproductById,
+  deleteProductById,
+} = require('../controllers/product.controller')
 
 router
   .route('/')
-  .get(async (req, res) => {
-    try {
-      const products = await Product.find()
-      res.json({ success: true, products })
-    } catch (err) {
-      res.status(500).json({
-        success: false,
-        message: 'unable to get products',
-        errorMessage: err.message,
-      })
-    }
-  })
-  .post(async (req, res) => {
-    const product = new Product(req.body)
-    try {
-      const savedProduct = await product.save()
-      res.json({ Product: savedProduct, success: true })
-    } catch (err) {
-      res.json({ success: false, message: err.message })
-    }
-  })
+  .get(getAllProducts)
+  .post(updateProducts)
 
-router.param('productId', async (req, res, next, productId) => {
+router
+  .param('productId', async (req, res, next, productId) => {
   try {
     const product = await Product.findById(productId)
 
@@ -47,34 +36,8 @@ router.param('productId', async (req, res, next, productId) => {
 
 router
   .route('/:productId')
-  .get((req, res) => {
-    const { product } = req
-    res.json({ success: true, product })
-  })
-  
-  .post(async (req, res) => {
-    const updatedProduct = req.body
-    let { product } = req
-
-    try {
-      product = extend(product, updatedProduct)
-      product = await product.save()
-      res.json({ success: true, product: product })
-    } catch (err) {
-      res.status(400).json({
-        success: false,
-        message: 'error while updating product',
-        error: err.message,
-      })
-    }
-  })
-
-  .delete(async (req, res) => {
-    let { product } = req
-
-    await product.remove()
-    product.deleted = true
-    res.json({ success: true, product })
-  })
+  .get(getProductById)
+  .post(updateproductById)
+  .delete(deleteProductById)
 
 module.exports = router
