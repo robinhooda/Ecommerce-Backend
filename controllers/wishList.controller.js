ef0591e7606b33ba414f755d0215ebdc57a2eea1
robinhooda <robinhooda66@gmail.com>
2021-06-05 01:55:55 +0530

    Refactor. added controller for wishlists routes

---
+++ controllers/wishList.controller.js
@@ -0,0 +1,70 @@
+const WishList = require('../models/WishList')
+const { extend } = require('lodash')
+
+const getAllWishLists = async (req, res) => {
+  try {
+    const wishLists = await WishList.find()
+      .populate('products', 'name price')
+      .select('_id products name')
+    res.json({ success: true, wishLists })
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: 'unable to get wishLists',
+      errorMessage: err.message,
+    })
+  }
+}
+
+const addNewWishList = async (req, res) => {
+  const wishList = new WishList(req.body)
+  try {
+    const savedWishList = await wishList.save()
+    res.json({ WishList: savedWishList, success: true })
+  } catch (err) {
+    res.json({
+      success: false,
+      message: 'error while adding wishList',
+      error: err.message,
+    })
+  }
+}
+
+const getWishListById = (req, res) => {
+  const { wishList } = req
+
+  res.json({ success: true, wishList })
+}
+
+const updateWishListById = async (req, res) => {
+  const updatedWishList = req.body
+  let { wishList } = req
+  try {
+    wishList.products = [...wishList.products, ...updatedWishList.products]
+
+    wishList = await wishList.save()
+    res.json({ success: true, wishList: wishList })
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: 'error while updating wishList',
+      error: err.message,
+    })
+  }
+}
+
+const deleteWishListById = async (req, res) => {
+  let { wishList } = req
+
+  await wishList.remove()
+  wishList.deleted = true
+  res.json({ success: true, wishList })
+}
+
+module.exports = {
+  getAllWishLists,
+  addNewWishList,
+  getWishListById,
+  updateWishListById,
+  deleteWishListById,
+}
--- routes/wishLists.router.js
+++ routes/wishLists.router.js
@@ -2,36 +2,18 @@ const express = require('express')
 const router = express.Router()
 const WishList = require('../models/WishList')
 const { extend } = require('lodash')
+const {
+  getAllWishLists,
+  addNewWishList,
+  getWishListById,
+  updateWishListById,
+  deleteWishListById,
+} = require('../controllers/wishList.controller')
 
 router
-  .route('/')
-  .get(async (req, res) => {
-    try {
-      const wishLists = await WishList.find()
-        .populate('products', 'name price')
-        .select('_id products name')
-      res.json({ success: true, wishLists })
-    } catch (err) {
-      res.status(500).json({
-        success: false,
-        message: 'unable to get wishLists',
-        errorMessage: err.message,
-      })
-    }
-  })
-  .post(async (req, res) => {
-    const wishList = new WishList(req.body)
-    try {
-      const savedWishList = await wishList.save()
-      res.json({ WishList: savedWishList, success: true })
-    } catch (err) {
-      res.json({
-        success: false,
-        message: 'error while adding wishList',
-        error: err.message,
-      })
-    }
-  })
+    .route('/')
+    .get(getAllWishLists)
+    .post(addNewWishList)
 
 router.param('wishListId', async (req, res, next, wishListId) => {
   try {
@@ -53,35 +35,8 @@ router.param('wishListId', async (req, res, next, wishListId) => {
 
 router
   .route('/:wishListId')
-  .get((req, res) => {
-    const { wishList } = req
-
-    res.json({ success: true, wishList })
-  })
-
-  .post(async (req, res) => {
-    const updatedWishList = req.body
-    let { wishList } = req
-    try {
-      wishList.products = [...wishList.products, ...updatedWishList.products]
-
-      wishList = await wishList.save()
-      res.json({ success: true, wishList: wishList })
-    } catch (err) {
-      res.status(400).json({
-        success: false,
-        message: 'error while updating wishList',
-        error: err.message,
-      })
-    }
-  })
-
-  .delete(async (req, res) => {
-    let { wishList } = req
-
-    await wishList.remove()
-    wishList.deleted = true
-    res.json({ success: true, wishList })
-  })
+  .get(getWishListById)
+  .post(updateWishListById)
+  .delete(deleteWishListById)
 
 module.exports = router
