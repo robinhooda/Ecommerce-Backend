dd46ccae4d4031120d49f1d5cc1ee76b66c92625
robinhooda <robinhooda66@gmail.com>
2021-06-05 02:20:05 +0530

    Refactor. added controller for category routes

--- routes/categories.router.js
+++ routes/categories.router.js
@@ -1,33 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const Category = require('../models/Category')
-const { extend } = require('lodash')
+const {
+  getAllCategories,
+  getCategoryById,
+  addNewCategory,
+  updateCategoryById,
+  deleteCategoryById,
+} = require('../controllers/category.controller')
 
 router
-  .route('/')
-  .get(async (req, res) => {
-    try {
-      const categories = await Category.find()
-        .populate('products', 'name price')
-        .select('_id products name')
-      res.json({ success: true, categories })
-    } catch (err) {
-      res.status(500).json({
-        success: false,
-        message: 'unable to get categories',
-        errorMessage: err.message,
-      })
-    }
-  })
-  .post(async (req, res) => {
-    const category = new Category(req.body)
-    try {
-      const savedCategory = await category.save()
-      res.json({ Category: savedCategory, success: true })
-    } catch (err) {
-      res.json({ success: false, message: err.message })
-    }
-  })
+    .route('/')
+    .get(getAllCategories)
+    .post(addNewCategory)
 
 router.param('categoryId', async (req, res, next, categoryId) => {
   try {
@@ -49,36 +34,8 @@ router.param('categoryId', async (req, res, next, categoryId) => {
 
 router
   .route('/:categoryId')
-  .get((req, res) => {
-    const { category } = req
-
-    category.__v = undefined
-    res.json({ success: true, category })
-  })
-
-  .post(async (req, res) => {
-    const updatedCategory = req.body
-    let { category } = req
-    try {
-      category = extend(category, updatedCategory)
-
-      category = await category.save()
-      res.json({ success: true, category: category })
-    } catch (err) {
-      res.status(400).json({
-        success: false,
-        message: 'error while updating category',
-        error: err.message,
-      })
-    }
-  })
-
-  .delete(async (req, res) => {
-    let { category } = req
-
-    await category.remove()
-    category.deleted = true
-    res.json({ success: true, category })
-  })
+  .get(getCategoryById)
+  .post(updateCategoryById)
+  .delete(deleteCategoryById)
 
 module.exports = router
