89f63cc855b867a0af5d1bbba8686fcfcf3b149d
robinhooda <robinhooda66@gmail.com>
2021-06-11 16:28:07 +0530

    Added routes at index.js

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,7 @@ const productsV1 = require('./products.router')
 const categoriesV1 = require('./categories.router')
 const wishListsV1 = require('./wishLists.router')
 const cartV1 = require('./cart.router')
+const loginV1 = require("./login.router")
+const signUpV1 = require("./signUp.router")
 
-module.exports = { productsV1, categoriesV1, wishListsV1, cartV1 }
+module.exports = { productsV1, categoriesV1, wishListsV1, cartV1, loginV1, signUpV1}
