e4248ad441500ff540225e0dbb42133c5ae18d7c
robinhooda <robinhooda66@gmail.com>
2021-06-11 16:42:06 +0530

    Fix. verifyToken  file name changed

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,6 @@
 const { errorHandler } = require('./error-handler.middleware')
 
 const { routeNotFound } = require('./route-not-found.middleware')
-const { auth } = require('./verifyToken')
+const { auth } = require('./verifyToken.middleware')
 
 module.exports = { errorHandler, routeNotFound, auth }
