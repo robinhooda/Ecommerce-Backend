5ab0230786114bc293277bab2bc205930094eccd
robinhooda <robinhooda66@gmail.com>
2021-06-11 16:25:49 +0530

    Add. auth middleware to wishlist and cart routes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
 
 const { initializeDB } = require('./db/db.connect.js')
 const {
@@ -9,9 +8,11 @@ const {
   categoriesV1,
   wishListsV1,
   cartV1,
+  loginV1,
+  signUpV1,
 } = require('./routes/index')
 
-const { errorHandler, routeNotFound } = require('./middlewares/index')
+const { errorHandler, routeNotFound, auth } = require('./middlewares/index')
 const PORT = process.env.PORT || 3020
 
 //connection to DB
@@ -20,10 +21,12 @@ initializeDB()
 app.use(express.json())
 app.use(cors())
 
+app.use('/login', loginV1)
+app.use('/signup', signUpV1)
 app.use('/products', productsV1)
 app.use('/categories', categoriesV1)
-app.use('/wishLists', wishListsV1)
-app.use('/cart', cartV1)
+app.use('/wishLists', auth, wishListsV1)
+app.use('/cart', auth, cartV1)
 
 app.get('/', (request, response) => {
   response.send('Hello World!')
